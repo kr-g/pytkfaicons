1370590c1fc7f877bc0d87bf36d741c9e3b05da5
kr-g <k.r.goger@gmail.com>
2022-06-13 06:05:46 +0200

    reworked

--- run_qs_tool.sh
+++ run_qs_tool.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-black .
-# flake8 --config flake8.cfg
-# python3 -m unittest -v
 
-for f in build dist *.egg-info; do 
+black .
+flake8 --config flake8.cfg
+python3 -m unittest -v
+
+
+for f in build, dist, *.egg-info, __pycache__ ; do 
     echo remove $f
-    rm $f -rf
+    find . -name $f | xargs rm -rf  
 done
 
-find . -name __pycache__ | xargs rm -rf  
