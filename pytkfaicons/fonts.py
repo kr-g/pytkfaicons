5452194e111268b22e859e83020cfe8c3840ffe6
kr-g <k.r.goger+github@gmail.com>
2022-07-31 09:38:17 +0200

    rework

--- pytkfaicons/fonts.py
+++ pytkfaicons/fonts.py
@@ -47,8 +47,8 @@ def create_font_icon(unicode_text, font, image_size, bg="white", fg="black"):
 
     height_2 = font.size / 2
 
-    sx = int(image_size[0] / 2 )
-    sy = int(image_size[1] / 2 )
+    sx = int(image_size[0] / 2)
+    sy = int(image_size[1] / 2)
 
     draw.text(xy=(sx, sy), text=unicode_text, font=font, fill=fg, anchor="mm")
 
